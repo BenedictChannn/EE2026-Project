23a4e8eacf230a0aefdfd2cefb74c82bcffd0361
Benedict Chan <benedictchan247@gmail.com>
2023-10-08 12:15:28 +0800

    Setup config files

---
+++ MODS/MODS.srcs/sources_1/new/Top_Student.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+//  FILL IN THE FOLLOWING INFORMATION:
+//  STUDENT A NAME: 
+//  STUDENT B NAME:
+//  STUDENT C NAME: 
+//  STUDENT D NAME:  
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top_Student (
+    // Delete this comment and include Basys3 inputs and outputs here
+    );
+
+    // Delete this comment and write your codes and instantiations here
+
+endmodule
